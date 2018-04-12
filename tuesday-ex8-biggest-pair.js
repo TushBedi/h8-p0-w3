c18c03b83cb6f830c7eed9dd78272154a5fc8690
Tushar <tusharbedi7373@hotmail.com>
2018-04-12 17:12:48 +0700

    updated algorith (removed sort, compared with 0)

--- tuesday-ex8-biggest-pair.js
+++ tuesday-ex8-biggest-pair.js
@@ -1,12 +1,16 @@
 function biggestPair (num) {
-  var pair = [];
+  // var pair = [];
+  var max = 0;
   var numStr = num.toString();
   for(var i = 0; i < numStr.length - 1; i++) {
-    pair.push(numStr.slice(i,i+2));
+    // pair.push(numStr.slice(i,i+2));
+    if (numStr.slice(i,i+2) > max) {
+      max = numStr.slice(i,i+2);
+    }
   } //return pair;
-    pair.sort();
-    var output = Number(pair[pair.length -1]);
-    return output;
+    // pair.sort();
+    // var output = Number(pair[pair.length -1]);
+    return max;
 }
 
 
